2daa03c3ed20655f3a93001cdd324e59ced944ee
Madaraka02 <victormadaraka@gmail.com>
2022-07-25 22:41:17 +0300

    added pysopg2

--- wash/settings.py
+++ wash/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1*nsz0h9ezo6(0teo0@@pb8^pwt=o09nvly&egv$+(fm_==557'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
