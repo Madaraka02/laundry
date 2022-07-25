3ede6ae5e786fdb61c1562382fb66562b6671c26
Madaraka02 <victormadaraka@gmail.com>
2022-07-26 01:35:37 +0300

    heroku debugs

--- clothes/models.py
+++ clothes/models.py
@@ -15,11 +15,7 @@ CONDITION_CHOICES = (
 
 class Client(models.Model):
     client_name = models.CharField(max_length=400, null=True, blank=True)
-    # check_in_date = models.DateTimeField(auto_now_add=True)
     check_in_date = models.DateTimeField(auto_now_add=True,null=True, blank=True)
-    # check_in_time = models.TimeField(auto_now_add=True,null=True, blank=True)
     picked = models.BooleanField(default=False)
     check_out_date = models.DateTimeField(auto_now=True,null=True, blank=True)
-    # check_out_time = models.TimeField(auto_now_add=True,null=True, blank=True)
-    # clothes_condition = models.CharField(max_length = 20,choices = CONDITION_CHOICES, default = 'GOOD')
     payment_method = models.CharField(max_length = 20,choices = PAYMENT_CHOICES, default = 'CASH')    
